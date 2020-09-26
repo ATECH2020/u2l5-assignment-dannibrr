7261b1af52aa788ee537f130f53adb5a76ddb897
Daniela Randolph <daniela.222144@nv.ccsd.net>
2020-09-26 19:55:36 +0000

    First run :)

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,37 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
-    }
-}
+      //creating a scanner object "scan"
+    Scanner scan = new Scanner(System.in);
+    //declaring variables that will be later used in the program
+    //and initlizaing variables that have constant values
+    double lumberCost = 8.0;
+    double windowCost = 11.0;
+    double total, taxes; 
+    int numBoards, numWindows;
+    
+    //asking the user a series of questions in order to initlize my variables 
+    System.out.println("Enter the sales tax rate:");
+    taxes = scan.nextDouble();
+    System.out.println("How many boards do you need?");
+    numBoards = scan.nextInt();
+    System.out.println("How many windows do you need?");
+    numWindows = scan.nextInt();
+
+    //creates an object "con" from the construction class 
+   //I will now be able to use methods within 
+    Construction con = new Construction(lumberCost, windowCost,taxes);
+
+    //using methods from the construction class to figure out my total
+    //also prints the total for the user
+    total = con.lumberCost(numBoards) + con.windowCost(numWindows);
+    System.out.println("Total: " + total);
+
+    //using the grandTotal method from the construction class to figure out the total + tax
+    //also prints the grand total for the user
+
+    System.out.println("Grand Total: " + con.grandTotal(total));
+
+
+    }//ends the main method
+}//ends the class 
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,33 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+      //creates a scanner object "scan"
+      Scanner scan = new Scanner(System.in);
+      //declaring varibles that will be needed for the program
+      double startLat, startLong, endLat, endLong, distance;
+
+      //initlizes varibles based on the user's response 
+      System.out.print("Enter the latitude of the starting location: ");
+      startLat = scan.nextDouble();
+      System.out.print("Enter the longitude of the starting location: ");
+      startLong = scan.nextDouble();
+      System.out.print("Enter the latitude of the ending location: ");
+      endLat = scan.nextDouble();
+      System.out.print("Enter the longitude of the ending location: ");
+      endLong = scan.nextDouble();
+
+      //creates a geolocation object "startGeo"
+      //responsible for the user's starting position
+      GeoLocation startGeo = new GeoLocation(startLat, startLong);
+
+      //creates a geolocation object "endGeo"
+      //responsible for the user's end position
+      GeoLocation endGeo = new GeoLocation(endLat, endLong);
+
+      //uses the distanceFrom(other Geo) method to calculate the distance
+      //the distance is then printed out using print :)
+      distance = startGeo.distanceFrom(endGeo);
+      System.out.print("The distance is " + distance + " miles.");
+
     }
 }
